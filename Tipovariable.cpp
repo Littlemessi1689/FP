8092450e4910db43dc560698299f699eaf2ce63c
Littlemessi1689 <jose.estradaM35@my.unitec.edu.mx>
2024-09-26 19:40:59 -0600

    Create Tipovariable.cpp

---
+++ Tipovariable.cpp
@@ -0,0 +1,25 @@
+// Practica2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// Bibiliotecas 
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	// 1. Entero (int) - 2 mil millones a 2 mil millones 
+	int numeroEntero = 42; // declaration 
+	cout << "Entero : " << numeroEntero << endl;
+
+	//2 Entero corto (short int) -32 768 a 32 767
+	short int numeroCorto = 32000;
+	cout << "Entero Corto : " << numeroCorto << endl;
+	// 3. Entero largo (long int)
+	long long numeroLargo = 8335313495L;
+	cout << "Entero largo : " << numeroLargo << endl;
+	// 4 unisgned 
+	unsigned numeroSinSigno = 400;
+	cout << "Numero : " << numeroSinSigno << endl;
+	//
+	unsigned short int 
+}
+	
