5920366cbf3d2f3207f337aebacde6d85b321cb6
Littlemessi1689 <jose.estradaM35@my.unitec.edu.mx>
2024-10-14 19:39:58 -0600

    Create practica_b.cpp

---
+++ practica_b.cpp
@@ -0,0 +1,20 @@
+// ConsoleApplication1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <iostream>
+using namespace std; 
+
+int main()
+
+{
+    
+    cout<< " (3 + 5 * 2 / 2 - 1) : " << (3 + 5 * 2 / 2 - 1) << endl;
+   
+
+    
+    
+    
+  
+}
+
