8f4eb136c80cd354b23ed62405f6614650c52e1b
Littlemessi1689 <jose.estradaM35@my.unitec.edu.mx>
2024-11-07 19:03:37 -0600

    Update practica_E.cpp

--- practica_E.cpp
+++ practica_E.cpp
@@ -18,35 +18,35 @@ int main()
 
 
 	//1.22 Hacer claculadora de operaciones aritemticas basicas 
-	string opciondeoperacion; 
-	int numero1, numero2, total; 
+	string opciondeoperacion;
+	int numero1, numero2, total;
 
 	do {
 		cout << "Menu de operaciones " << endl;
-		cout << endl; 
-	
-		cout << "a) Suma de dos operandos" << endl; 
+		cout << endl;
+
+		cout << "a) Suma de dos operandos" << endl;
 		cout << "b) resta de dos operandos" << endl;
 		cout << "c) multiplicacion de dos operandos" << endl;
 		cout << "d) division de dos operandos" << endl;
 		cout << "e) residuo de dos operandos" << endl;
 		cout << "s) salir del menu " << endl;
-		cout << endl; 
-		cout << "Seleccione una opcion "; 
-		cin >> opciondeoperacion; 
-		
+		cout << endl;
+		cout << "Seleccione una opcion ";
+		cin >> opciondeoperacion;
+
 		if (opciondeoperacion != "s")
 		{
-			cout << "Ingrese el primer numero : "; 
-			cin >> numero1; 
-			cout << "Ingrese el segundo numero : "; 
-			cin >> numero2; 
+			cout << "Ingrese el primer numero : ";
+			cin >> numero1;
+			cout << "Ingrese el segundo numero : ";
+			cin >> numero2;
 		}
 		if (opciondeoperacion == "a")
 		{
-			total = numero1 + numero2; 
-			cout << "La suma de los numeros es : " << total << endl; 
-		 
+			total = numero1 + numero2;
+			cout << "La suma de los numeros es : " << total << endl;
+
 		}
 		if (opciondeoperacion == "b")
 		{
@@ -60,7 +60,7 @@ int main()
 			cout << "La multiplicacion de los numeros es : " << total << endl;
 
 		}
-		if (opciondeoperacion == "d" and numero2 !=0 )
+		if (opciondeoperacion == "d" and numero2 != 0)
 		{
 			total = numero1 / numero2;
 			cout << "division de los numeros es : " << total << endl;
@@ -68,69 +68,103 @@ int main()
 		}
 		if (opciondeoperacion == "e" and numero2 != 0)
 		{
-			total = numero1 % numero2; 
-			cout << "El residuo de los numero es : " << total << endl; 
-		} 
-		
-		numero1 = 0, numero2 = 0, total = 0; 
-	} while (opciondeoperacion != "s"); 
+			total = numero1 % numero2;
+			cout << "El residuo de los numero es : " << total << endl;
+		}
+
+		numero1 = 0, numero2 = 0, total = 0;
+	} while (opciondeoperacion != "s");
 
 	// 1.23 Simulacion de cajero 
-	double saldo = 3450; 
-	int pin = 1234; 
-	cout << "Ingrese su pin de 4 digitos por favor "; 
-	cin >> pin; 
+	double saldo = 3450;
+	int pin = 1234;
+	cout << "Ingrese su pin de 4 digitos por favor ";
+	cin >> pin;
 	do
 	{
 		if (pin != 1234)
 		{
-			cout << "El pin es incorrecto "; 
-				break; 
+			cout << "El pin es incorrecto ";
+			break;
 		}
 		if (pin == 1234)
-			cout << "pin correcto" << endl; 
+			cout << "pin correcto" << endl;
 
-	} while (pin != 1234); 
-	
-	char opcioncajero; 
-	
+	} while (pin != 1234);
+
+	char opcioncajero;
+
 	do
 	{
-		cout << "Menu de cajero de operaciones unitec" << endl; 
-		cout << "a) consulta de saldo " << endl; 
-		cout << "b) retiro de dinero " << endl; 
-		cout << "c) canelar operacion " << endl; 
-		cout << "Escoja una opcion "; 
-		cin >> opcioncajero; 
+		cout << "Menu de cajero de operaciones unitec" << endl;
+		cout << "a) consulta de saldo " << endl;
+		cout << "b) retiro de dinero " << endl;
+		cout << "c) canelar operacion " << endl;
+		cout << "Escoja una opcion ";
+		cin >> opcioncajero;
 		if (opcioncajero == 'a')
 		{
 			cout << "Su saldo es 3450.00 pesos ";
-			break; 
+			break;
 		}
 		if (opcioncajero == 'b')
 		{
-			double cantidadretiro; 
-			cout << "Su saldo es de 3450.00 pesos por favor elija la cantidad a retirar "; 
-			cin >> cantidadretiro; 
-			if (cantidadretiro> saldo)
+			double cantidadretiro;
+			cout << "Su saldo es de 3450.00 pesos por favor elija la cantidad a retirar ";
+			cin >> cantidadretiro;
+			if (cantidadretiro > saldo)
 			{
-				cout << "fondos insuficientes"; 
-				break; 
-			
+				cout << "fondos insuficientes";
+				break;
+
 			}
 			if (cantidadretiro <= saldo)
 			{
-				cout << "A retirado exitosamente " << cantidadretiro << "pesos, su saldo restante es de : " << saldo - cantidadretiro << "pesos"; 
-				break; 
+				cout << "A retirado exitosamente " << cantidadretiro << "pesos, su saldo restante es de : " << saldo - cantidadretiro << "pesos";
+				break;
 			}
 
 		}
 
 	} while (opcioncajero != 'c');
+
+	 //1.24 Temple run 
+
+	srand(time(NULL)); 
+	int castigo, premio; 
+	int vida = 12; 
+	cout << "vida inicial " << vida<< endl; 
 	
-
+	while (vida >0)
+	{
+		//consume vida 
+		vida--; 
+		cout << "Consumo " << vida << endl; 
+		castigo = rand() % 2; 
+		if (castigo ==1)
+		{
+			vida = vida - 3; 
+			cout << "Castigo " << vida << endl;
+		}
+		premio = rand() % 2; 
+		if (premio == 1)
+		{
+			vida = vida + 3; 
+			cout << "premio " << vida << endl;
+		}
+		cout << "fin ciclo  " << vida << endl;
 		
+	}
 
+
+
+
+
+
+
+
+
+}
 
 	
 
